b636da9a54003b672ccd1b0f43485af00d185a71
Pierre Chanial <pierre.chanial@apc.in2p3.fr>
2022-04-30 22:15:27 +0000

    Don't remove volumes in rm.sh.

--- compose/rm.sh
+++ compose/rm.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env sh
 
 export SONOUNO_PATH=$(dirname "$(readlink -f "$0")")
-docker-compose -f ${SONOUNO_PATH}/docker-stack.yml down -v --remove-orphans
+docker-compose -f ${SONOUNO_PATH}/docker-stack.yml down --remove-orphans "$@"
 docker network prune --force
