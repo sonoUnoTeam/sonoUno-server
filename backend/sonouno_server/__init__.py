9be85b23d592da01bc24e188f3b64f67ffab3c2e
Pierre Chanial <pierre.chanial@apc.in2p3.fr>
2022-08-28 00:00:30 +0200

    Don't rely on a local file for the package version.

--- backend/sonouno_server/__init__.py
+++ backend/sonouno_server/__init__.py
@@ -4,11 +4,3 @@ __all__ = [
 
 # version set by poetry build though poetry-dynamic-version-plugin
 __version__ = '0.0.0'
-
-# for local development, we use the file _version.py which is ignored by git
-try:
-    from ._version import __version__
-except ImportError:
-    pass
-
-__version__ = '0.4.0'  # noqa
