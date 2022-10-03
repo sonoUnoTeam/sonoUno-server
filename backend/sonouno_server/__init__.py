6242f34ec395879d30c38d71d9cda2a056d60946
Pierre Chanial <pierre.chanial@apc.in2p3.fr>
2022-10-03 09:59:31 +0200

    Update for Poetry 1.2.

--- backend/sonouno_server/__init__.py
+++ backend/sonouno_server/__init__.py
@@ -2,5 +2,5 @@ __all__ = [
     '__version__',
 ]
 
-# version set by poetry build though poetry-dynamic-version-plugin
+# version set by poetry build though the poetry-dynamic-version plugin
 __version__ = '0.0.0'
